e17e239651868c56509c67dacd54386cb464fa85
samiyaazizxonova <samiyaazizxonova2000@gmail.com>
2023-01-01 14:33:16 +0500

    new site

---
+++ app.js
@@ -0,0 +1,27 @@
+let h2 = document.querySelector('#h2')
+let input = document.querySelector('#input')
+const scoreEl = document.querySelector('#score')
+input.focus()
+
+// global
+let randomWord
+
+let score = 0
+
+
+function generateRandomWord() {
+  const randomNumber = Math.trunc(Math.random() * words.length)
+  randomWord = words[randomNumber]
+  h2.textContent = (words[randomNumber])
+}
+generateRandomWord()
+
+input.addEventListener('input', ()=> {
+  if(randomWord == input.value){
+    console.log('other')
+    generateRandomWord()
+    score++
+    scoreEl.innerHTML = `Score : ${score}`
+    input.value = ''
+  }
+})
